368c69c35b2126b8a1e2b73bfc5d9a0753dde545
fire8327 <fire83274@gmail.com>
2024-06-13 18:57:30 +0300

    upd

--- pages/catalog/product-[id].vue
+++ pages/catalog/product-[id].vue
@@ -3,8 +3,8 @@
         <img src="/images/products/1.jpg" alt="" class="w-full lg:w-1/2 rounded-xl object-cover aspect-video h-fit">
         <div class="flex flex-col gap-6 w-full lg:w-1/2 p-4 rounded-xl bg-white/5 border border-white/15 backdrop-blur-3xl">
             <p class="text-2xl font-Comfortaa font-semibold tracking-widest">{{ products[0].title }}</p>
-            <div class="flex flex-col gap-2">
-                <div class="flex items-center gap-2 w-full" v-for="(value, key) in products[0].characteristics">
+            <div class="flex flex-col gap-8 md:gap-2">
+                <div class="flex items-center max-md:flex-col max-md:text-center gap-2 w-full" v-for="(value, key) in products[0].characteristics">
                     <p class="w-fit">{{ key }}</p>
                     <div class="grow h-px border-b border-white border-dashed min-w-[50%]"></div>
                     <p class="w-fit">{{ value }}</p>
